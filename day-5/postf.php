355e3bc618a7129c6bed6bb625d131bb698c60ea
Rajat Bhansali <rajatbhansali01@gmail.com>
2018-10-04 23:38:37 +0530

    Using POST method

---
+++ day-5/postf.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="dispost.php" method="POST">
+Name<input type="text" name="name">
+course<input type="text" name="course">
+
+<input type="SUBMIT" >
+</form>
+</body>
+</html>
